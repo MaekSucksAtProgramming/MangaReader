26cd1906fb07eac6d7cdb7cef68fd09cd54cc08f
Anon1066 <diego.lobat86@gmail.com>
2017-10-21 13:07:40 -0200

    Updated example images

--- src/js/renderer.js
+++ src/js/renderer.js
@@ -25,11 +25,19 @@ ipc.on('selected-directory', function (event, files) {
 /////////////////////////////////////////////
 
 let imagesArray = [
-    '../assets/images/1.png',
-    '../assets/images/2.png',
-    '../assets/images/3.png',
+    '../assets/images/1.jpg',
+    '../assets/images/2.jpg',
+    '../assets/images/3.jpg',
     '../assets/images/4.jpg',
-    '../assets/images/5.jpg'
+    '../assets/images/5.jpg',
+    '../assets/images/6.jpg',
+    '../assets/images/7.jpg',
+    '../assets/images/8.jpg',
+    '../assets/images/9.jpg',
+    '../assets/images/10.jpg',
+    '../assets/images/11.jpg',
+    '../assets/images/12.jpg',
+    '../assets/images/13.jpg'
 ]
 
 const image = document.getElementById('image-viewer');
